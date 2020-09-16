62d739b1f4752df8d8fe1f9387110374aea68234
pichuzhkinaen <88-katerina@mail.ru>
2020-09-16 21:56:34 +0300

    add our company, map, form, footer, search(html)

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+    'use strict';
 
+    // let gridItem = document.getElementById('catalog-grid').children,
+    //     textItem = document.getElementById('catalog-text').children;
+
+    // console.log(textItem);
+
+    // for (let i = 0; i < gridItem.length; i++) {
+    //     gridItem[i].innerHTML = textItem[i].innerHTML;
+    // }
+
+}); 
